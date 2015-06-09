5157547e541fa94618e512592521ea4ee365d97d
Alex Wilhelm <awwilhelm2014@gmail.com>
2015-06-09 13:37:41 -0500

    Added shader for power on the arrow
    Added 10 colors
    Added blender
    Added material via script to avoid uninitialized error

--- Assets/Mini-Golf-Topo/Scripts/ControlBall.cs
+++ Assets/Mini-Golf-Topo/Scripts/ControlBall.cs
@@ -10,6 +10,7 @@ public class ControlBall : MonoBehaviour {
 	public RectTransform arrowTail;
 	public GameObject camera3D;
 	public GameObject scoreKeepingObject;
+	public Material arrowTailShader;
 	private ScoreKeeping scoreKeepingScript;	
 	private BallForce bfScript;
 	private CameraFollow cameraFollowScript;
@@ -17,6 +18,7 @@ public class ControlBall : MonoBehaviour {
 
 	//Deals with mouse position on screen and where it is
 	private float distance;
+	private float arrowScalePercent;
 	private Vector3 v3_transform;
 
 	//Used for scaling and how much power to use to hit the ball
@@ -29,8 +31,7 @@ public class ControlBall : MonoBehaviour {
 	private const float ARROW_HEAD_SCALE_COEFFICIENT = 3;
 	private const float ARROW_TAIL_SCALE_COEFFICIENT = 2;
 	private const float ARROW_TAIL_MOUSE_OFFEST = 6;
-
-	// Use this for initialization
+	
 	void Start () {
 		arrowHead.sizeDelta = new Vector2(arrowHead.rect.width * ARROW_HEAD_SCALE_COEFFICIENT, arrowHead.rect.height * ARROW_HEAD_SCALE_COEFFICIENT);
 		arrowTail.sizeDelta = new Vector2(arrowTail.rect.width, arrowTail.rect.height * ARROW_TAIL_SCALE_COEFFICIENT);
@@ -38,7 +39,9 @@ public class ControlBall : MonoBehaviour {
 		bfScript = GetComponent<BallForce> ();
 		cameraFollowScript = camera3D.GetComponent<CameraFollow>();
 		arrowTailRenderer = arrowTail.GetComponent<CanvasRenderer>();
+		arrowTailRenderer.SetMaterial(arrowTailShader, null);
 		distance = 0;
+		arrowScalePercent = 0;
 	}
 	
 	// Update is called once per frame
@@ -52,8 +55,7 @@ public class ControlBall : MonoBehaviour {
 				scoreKeepingScript.addToHits();
 
 				//Scales arrow scale percent to force
-				float arrowScalePercent, force, forceCurve;
-				arrowScalePercent = (distance-MIN_ARROW_SCALE)/(MAX_ARROW_SCALE-MIN_ARROW_SCALE);
+				float force, forceCurve;
 				force = arrowScalePercent*(MAX_POWER-MIN_POWER) + MIN_POWER;
 
 				forceCurve = 2750 * force + 300;
@@ -107,25 +109,23 @@ public class ControlBall : MonoBehaviour {
 		canvasArrow.transform.rotation = Quaternion.LookRotation(new Vector3(v3T.x, v3T.y, v3T.z));
 		canvasArrow.transform.rotation = Quaternion.Euler(new Vector3(270, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z-90));
 
-
-
-		if(distance<=MAX_ARROW_SCALE && distance>=MIN_ARROW_SCALE)
+		if(distance<MAX_ARROW_SCALE && distance>MIN_ARROW_SCALE)
 		{
 			arrowTail.sizeDelta = new Vector2(distance - ARROW_TAIL_MOUSE_OFFEST, arrowTail.rect.height);
-			arrowTailRenderer.GetMaterial().SetFloat("_TexWidth",  distance - ARROW_TAIL_MOUSE_OFFEST);
-			//print (arrowTailRenderer.GetMaterial().GetFloat("_TexWidth"));
-			//arrowTailRenderer.material.SetFloat("_TexWidth",  distance - ARROW_TAIL_MOUSE_OFFEST);
+
 		}
-		else if(distance>MAX_ARROW_SCALE)
+		else if(distance>=MAX_ARROW_SCALE)
 		{
 			arrowTail.sizeDelta = new Vector2(MAX_ARROW_SCALE - ARROW_TAIL_MOUSE_OFFEST, arrowTail.rect.height);
 			distance = MAX_ARROW_SCALE;
 		}
-		else if(distance<MIN_ARROW_SCALE)
+		else if(distance<=MIN_ARROW_SCALE)
 		{
 			arrowTail.sizeDelta = new Vector2(MIN_ARROW_SCALE - ARROW_TAIL_MOUSE_OFFEST, arrowTail.rect.height);
 			distance = MIN_ARROW_SCALE;
 		}
+		arrowScalePercent = (distance-MIN_ARROW_SCALE)/(MAX_ARROW_SCALE-MIN_ARROW_SCALE);
+		arrowTailRenderer.GetMaterial().SetFloat("_TexWidth",  arrowScalePercent);
 	}
 
 }
--- Assets/Mini-Golf-Topo/Shader/ArrowShader.shader
+++ Assets/Mini-Golf-Topo/Shader/ArrowShader.shader
@@ -1,11 +1,19 @@
 ﻿Shader "Custom/ArrowShader" {
 	Properties {
         _MainTex ("Base (RGB)", 2D) = "white" {}
-        [HideInInspector]_TexWidth ("Texture Size", Float) = 512
-        _test1 ("test1", Color) = (1,1,1,1)
-        _test2 ("test2", Color) = (0.9, 0.8, 0.7, 1)
-        _test3 ("test3", Color) = (0.2, 0.3, 0.4, 1)
-        _worldPos("worldPos", Float) = 0.5
+        [HideInInspector]_TexWidth ("Texture Size", float) = 50
+        
+        _lowest10 ("Lowest 10", Color) = (1,1,1,1)
+        _lowest20 ("Lowest 20", Color) = (0.9, 0.8, 0.7, 1)
+        _lowest30 ("Lowest 30", Color) = (0.2, 0.3, 0.4, 1)
+        _lowest40 ("Lowest 40", Color) = (1,1,1,1)
+        _lowest50 ("Lowest 50", Color) = (0.9, 0.8, 0.7, 1)
+        _lowest60 ("Lowest 60", Color) = (0.2, 0.3, 0.4, 1)
+        _lowest70 ("Lowest 70", Color) = (1,1,1,1)
+        _lowest80 ("Lowest 80", Color) = (0.9, 0.8, 0.7, 1)
+        _lowest90 ("Lowest 90", Color) = (0.2, 0.3, 0.4, 1)
+        _lowest100 ("Lowest 100", Color) = (0.2, 0.3, 0.4, 1)
+        _blendVal ("Blend Value", Range(0,1)) = 1
     }
     SubShader {
         Tags { "RenderType"="Opaque" }
@@ -16,10 +24,18 @@
 
         sampler2D _MainTex;
         float _TexWidth;
-        float4 _test1;
-        float4 _test2;
-        float4 _test3;
-        float _worldPos;
+        float _TexMaxWidth;
+        half3 _lowest10;
+        half3 _lowest20;
+        half3 _lowest30;
+        half3 _lowest40;
+        half3 _lowest50;
+        half3 _lowest60;
+        half3 _lowest70;
+        half3 _lowest80;
+        half3 _lowest90;
+        half3 _lowest100;
+        float _blendVal;
 
         struct Input {
             float2 uv_MainTex;  
@@ -31,13 +47,26 @@
 	    }
 
         void surf (Input IN, inout SurfaceOutput o) {
-            //o.Albedo = Color.blue;
-            _worldPos=IN.worldPos.x;
-            o.Albedo = _test3;
-            if(_TexWidth < 20)
-            	o.Albedo = _test1;
+            if(_TexWidth < 0.10)
+            	o.Albedo = _lowest10;  
+            else if(_TexWidth < 0.20)
+            	o.Albedo = lerp(_lowest10, _lowest20, _blendVal);
+            else if(_TexWidth < 0.30)
+            	o.Albedo = lerp(_lowest20, _lowest30, _blendVal);
+            else if(_TexWidth < 0.40)
+            	o.Albedo = lerp(_lowest30, _lowest40, _blendVal);
+            else if(_TexWidth < 0.50)
+            	o.Albedo = lerp(_lowest40, _lowest50, _blendVal);
+            else if(_TexWidth < 0.60)
+            	o.Albedo = lerp(_lowest50, _lowest60, _blendVal);
+           	else if(_TexWidth < 0.70)
+            	o.Albedo = lerp(_lowest60, _lowest70, _blendVal);
+            else if(_TexWidth < 0.80)
+            	o.Albedo = lerp(_lowest70, _lowest80, _blendVal);
+            else if(_TexWidth < 0.90)
+            	o.Albedo = lerp(_lowest80, _lowest90, _blendVal);
             else
-            	o.Albedo = _test2;
+            	o.Albedo = lerp(_lowest90, _lowest100, _blendVal);
         }
         ENDCG
     } 
